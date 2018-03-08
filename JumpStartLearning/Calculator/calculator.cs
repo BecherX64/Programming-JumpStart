c0a43809f8e980a28ad78b2cea6dba37c8be5a8c
Batis, Ivan <ivan.batis@gmail.com>
2018-03-08 15:03:46 +0100

    Commit: Calc added 2018-03-08 (initial)

---
+++ JumpStartLearning/Calculator/calculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+
+	public class calculator
+	{
+		public static void Main()
+		{
+			//SomeOperation add = Sum;
+			char key;
+			bool AppStatus = true;
+			int Number1, Number2;
+			do
+			{
+				ShowMenu();
+				key = Console.ReadKey(true).KeyChar;			
+				switch (key)
+				{
+					case 'a':
+						Number1 = GetSomeKeyAsNumber();
+						Number2 = GetSomeKeyAsNumber();
+						int AddResult = Addition(Number1, Number2);
+						Console.WriteLine("Result: {0} + {1} = {2}", Number1, Number2, AddResult);
+						break;
+					case 's':
+						Number1 = GetSomeKeyAsNumber();
+						Number2 = GetSomeKeyAsNumber();
+						int SubResult = Substraction(Number1, Number2);
+						Console.WriteLine("Result: {0} - {1} = {2}", Number1, Number2, SubResult);
+						break;
+					case 'c':
+						Console.Clear();
+						break;
+					default:
+						//Console.WriteLine("no action");
+						AppStatus = false;
+						break;
+				}
+
+			} while (AppStatus);
+			Console.WriteLine("Press any key ...");
+			Console.ReadKey(true);
+		}
+
+		static int Substraction(int i, int j)
+		{
+			return i - j;
+		}
+
+		static int Addition(int i, int j)
+		{
+			return i + j;
+		}
+
+		static int GetSomeKeyAsNumber()
+		{
+			Console.WriteLine("Enter any number:");
+			string _Text = Console.ReadLine();
+			Int32.TryParse(_Text, out int _output);
+			return _output;
+			
+		}
+		static void ShowMenu()
+		{
+			//Console.Clear();
+			Console.WriteLine("(a)ddition: Press 'a'");
+			Console.WriteLine("(s)ubtraction: Press 's'");
+			Console.WriteLine("E(x)it: Press 'x'");
+		}
+	}
+}
---
+++ JumpStartLearning/JumpStartLearning/Learning.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpStartLearning
+{
+	class Learning
+	{
+		static void Main(string[] args)
+		{
+		    bool AppStatus = true;
+			char key;
+			Console.WriteLine("I am good");
+			
+			do
+			{	
+				key = Console.ReadKey(true).KeyChar;
+				Console.WriteLine();
+				Console.WriteLine("You typed:{0}", key);
+
+				if (key.Equals('x') )	
+				{
+					AppStatus = false;
+				}
+			} while (AppStatus);
+			Console.WriteLine("Press any key ..");
+			Console.ReadKey(true);
+		}
+	}
+}
